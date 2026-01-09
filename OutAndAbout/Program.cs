b6b99dae47231768c6f861b84dbbd3e2f0cd199c
Lene <Lene-tl@hotmail.com>
2026-01-09 13:59:43 +0100

    init

---
+++ OutAndAbout/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OutAndAbout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Lars!");
+        }
+    }
+}
